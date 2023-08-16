5b630a31996870ee0090a6743f9dbfc89779d27c
Daniel Brai <daniel.gm.brai@gmail.com>
2023-08-16 20:31:26 -0300

    build: estrutura o projeto
    cria o projeto utilizando o start.spring.io.

---
+++ src/main/java/com/danielbrai/gymtonic/GymTonicApplication.java
@@ -0,0 +1,13 @@
+package com.danielbrai.gymtonic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymTonicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymTonicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danielbrai/gymtonic/GymTonicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielbrai.gymtonic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymTonicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
