6a145a7d1167b1bf1581f3a5fc9eadaa20afee06
Daniel Brai <daniel.gm.brai@gmail.com>
2023-08-17 06:43:29 -0300

    build: corrige a tabela de exercicios
    adiciona a coluna numero_repeticoes na tabela de exercicios, a torna seus campos obrigatorios com excecao da coluna carga

--- src/main/resources/infraestructure/database/create_table_exercicio.sql
+++ src/main/resources/infraestructure/database/create_table_exercicio.sql
@@ -1,7 +1,9 @@
+DROP TABLE IF EXISTS exercicio;
 CREATE TABLE IF NOT EXISTS exercicio (
-    id SERIAL PRIMARY KEY,
-    nome varchar(150),
-    numero_series smallint,
-    carga smallint,
-    intervalo_segundos smallint
-    );
+    id SERIAL PRIMARY KEY NOT NULL,
+    nome VARCHAR(150) NOT NULL,
+    numero_series SMALLINT NOT NULL,
+    carga SMALLINT,
+    intervalo_segundos SMALLINT NOT NULL,
+    numero_repeticoes SMALLINT NOT NULL
+);
--- src/main/resources/infraestructure/database/insert_data_exercicio.sql
+++ src/main/resources/infraestructure/database/insert_data_exercicio.sql
@@ -1,10 +1,10 @@
 INSERT INTO gym_tonic.exercicio
-(nome, numero_series, carga, intervalo_segundos)
+(nome, numero_series, numero_repeticoes, carga, intervalo_segundos)
 values
-('Agachamento A Fundo', 4, 12, 5),
-('Cadeira Extensora', 4, 12, 35),
-('Leg Press Horizontal', 4, 12, 85),
-('Gêmeos Sentado', 4, 12, 25),
-('Triceps Barra Reta', 5, 12, 30),
-('Elevação Lateral com Halter', 5, 12, 5),
-('Rosca Direta com Halter', 5, 12, 8);
+('Agachamento A Fundo', 4, 12, 5, 60),
+('Cadeira Extensora', 4, 12, 35, 60),
+('Leg Press Horizontal', 4, 12, 85, 60),
+('Gêmeos Sentado', 4, 12, 25, 60),
+('Triceps Barra Reta', 5, 12, 30, 90),
+('Elevação Lateral com Halter', 5, 12, 5, 90),
+('Rosca Direta com Halter', 5, 12, 8, 90);
