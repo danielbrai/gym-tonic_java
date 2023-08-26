115aa65c735239a6696c961b6f9c0dd97d6e6b9f
Daniel Brai <daniel.gm.brai@gmail.com>
2023-08-26 18:49:35 -0300

    build: imlementa endpoint de teste

--- src/main/java/com/danielbrai/gymtonic/GymTonicApplication.java
+++ src/main/java/com/danielbrai/gymtonic/GymTonicApplication.java
@@ -2,6 +2,8 @@ package com.danielbrai.gymtonic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class GymTonicApplication {
@@ -11,3 +13,12 @@ public class GymTonicApplication {
 	}
 
 }
+@RestController
+class GreetingController {
+
+	@GetMapping(value = "/greet")
+	public String greet() {
+		return "Greeting from Railway.app";
+	}
+}
+
